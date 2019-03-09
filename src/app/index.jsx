a57ada0e01a16587ad26adccd022f6121a72697d
Giorgio Acosta <gioacostax@gmail.com>
2019-03-09 17:20:34 -0500

    [Redux] Initial commit :doughnut:

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2015-2018 gioacostax. All rights reserved.
+ * Copyright © 2015-2019 gioacostax. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -10,23 +10,13 @@ import './styles.scss';
 import Redux, { database, counter } from 'src/redux';
 import { Loader } from 'src/components';
 
-@Redux({ database, counter })
-export default class App extends React.Component {
+export default @Redux({ database, counter }) class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      result: ''
-    };
-
     this.getData = () => {
-      this.setState({ result: '[app-Web] Cargando...' });
       this.props.counter.add(1);
-      this.props.database.getData('/manifest.json').then((res) => {
-        this.setState({ result: `[app-Web] Last action: ${res.type} OK.` });
-      }).catch((err) => {
-        this.setState({ result: `[app-Web] Error: ${err.message}` });
-      });
+      this.props.database.getData('/manifest.json');
     };
   }
 
@@ -38,7 +28,7 @@ export default class App extends React.Component {
         </div>
         <div id="app-body">
           <h3>REDUX (database.state)</h3>
-          <div className="state-box">{JSON.stringify(this.props.database.state)}</div>
+          <div className="state-box">{JSON.stringify(this.props.database.state.data)}</div>
           {
             this.props.database.state.loading
               ? <Loader color="#888" />
@@ -48,7 +38,7 @@ export default class App extends React.Component {
                 </button>
               )
           }
-          <div className="result-message">{this.state.result}</div>
+          <div className="result-message">{this.props.database.state.status}</div>
         </div>
         <div id="app-footer">
           <h4>Made with <span className="heart">♥</span> by <a href="https://github.com/gioacostax">@gioacostax</a></h4>
